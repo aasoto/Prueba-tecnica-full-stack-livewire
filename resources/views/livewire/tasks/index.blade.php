31b4c62ce8539425b578abf51eac5c93663798a0
aasoto <andresalfredosotosuarez@gmail.com>
2023-08-30 17:44:31 -0500

    feat: Validate not found registers

--- resources/views/livewire/tasks/index.blade.php
+++ resources/views/livewire/tasks/index.blade.php
@@ -90,76 +90,82 @@
             </div>
         </div>
     </div>
-    <table class="w-full">
-        <thead>
-            <tr>
-                <th class="bg-gray-300 rounded-tl-2xl font-bold px-2 py-5">
-                    ID
-                </th>
-                <th class="bg-gray-300 font-bold px-2 py-5">
-                    Hacer
-                </th>
-                <th class="bg-gray-300 font-bold px-2 py-5">
-                    Completado
-                </th>
-                <th class="bg-gray-300 rounded-tr-2xl font-bold px-2 py-5">
-                    Acciones
-                </th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($tasks as $task)
-                <tr class="border-b border-gray-400">
-                    <td class="font-bold pl-4 py-2 text-center">
-                        {{ $task->id }}
-                    </td>
-                    <td class="capitalize pl-4 py-2">
-                        <span
-                            :class="(editing == true) && (item == {{ $task->id }}) ? 'hidden': 'block'"
-                            @click="edit({{$task->id}}, '{{$task->todo}}')"
-                        >
-                            {{ $task->todo }}
-                        </span>
-                        <template x-if="editing && item == {{ $task->id }}">
-                            <input
-                                :value="todo"
-                                type="text"
-                                class="border border-gray-500 rounded-md px-5 py-2 placeholder:italic w-full"
-                                placeholder="Editar tarea"
-                                @keyup.enter="update($event.target.value)"
-                            />
-                        </template>
-                    </td>
-                    <td class="capitalize pl-4 py-2 text-center">
-                        @if ($task->done)
-                            <input
-                                checked
-                                type="checkbox"
-                                value=""
-                                wire:click="completed({{$task}})"
-                                class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                            >
-                        @else
-                            <input
-                                type="checkbox"
-                                value=""
-                                wire:click="completed({{$task}})"
-                                class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-                            >
-                        @endif
-                    </td>
-                    <td class="capitalize pl-4 py-2 flex justify-center items-center">
-                        <button wire:click="selectedTaskToDelete({{ $task }})" class="bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded-md shadow-sm hover:shadow scale-100 hover:scale-105 transition duration-200">
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-                                <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
-                            </svg>
-                        </button>
-                    </td>
+    @if (count($tasks))
+        <table class="w-full">
+            <thead>
+                <tr>
+                    <th class="bg-gray-300 rounded-tl-2xl font-bold px-2 py-5">
+                        ID
+                    </th>
+                    <th class="bg-gray-300 font-bold px-2 py-5">
+                        Hacer
+                    </th>
+                    <th class="bg-gray-300 font-bold px-2 py-5">
+                        Completado
+                    </th>
+                    <th class="bg-gray-300 rounded-tr-2xl font-bold px-2 py-5">
+                        Acciones
+                    </th>
                 </tr>
-            @endforeach
-        </tbody>
-    </table>
-    {{ $tasks->links() }}
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                    <tr class="border-b border-gray-400">
+                        <td class="font-bold pl-4 py-2 text-center">
+                            {{ $task->id }}
+                        </td>
+                        <td class="capitalize pl-4 py-2">
+                            <span
+                                :class="(editing == true) && (item == {{ $task->id }}) ? 'hidden': 'block'"
+                                @click="edit({{$task->id}}, '{{$task->todo}}')"
+                            >
+                                {{ $task->todo }}
+                            </span>
+                            <template x-if="editing && item == {{ $task->id }}">
+                                <input
+                                    :value="todo"
+                                    type="text"
+                                    class="border border-gray-500 rounded-md px-5 py-2 placeholder:italic w-full"
+                                    placeholder="Editar tarea"
+                                    @keyup.enter="update($event.target.value)"
+                                />
+                            </template>
+                        </td>
+                        <td class="capitalize pl-4 py-2 text-center">
+                            @if ($task->done)
+                                <input
+                                    checked
+                                    type="checkbox"
+                                    value=""
+                                    wire:click="completed({{$task}})"
+                                    class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                >
+                            @else
+                                <input
+                                    type="checkbox"
+                                    value=""
+                                    wire:click="completed({{$task}})"
+                                    class="w-6 h-6 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+                                >
+                            @endif
+                        </td>
+                        <td class="capitalize pl-4 py-2 flex justify-center items-center">
+                            <button wire:click="selectedTaskToDelete({{ $task }})" class="bg-red-500 hover:bg-red-600 text-white font-bold p-2 rounded-md shadow-sm hover:shadow scale-100 hover:scale-105 transition duration-200">
+                                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                                    <path stroke-linecap="round" stroke-linejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0" />
+                                </svg>
+                            </button>
+                        </td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{ $tasks->links() }}
+    @else
+        <div class="w-full my-5 px-10 py-5 border border-blue-600 bg-blue-300 rounded-lg text-blue-700 text-center">
+            No se encontraron tareas
+        </div>
+    @endif
 </div>
 <script>
     const data = () => {
